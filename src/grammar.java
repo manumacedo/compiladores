cfa209363d75fb22c019d6fcea4ae6b045623e3f
Manuelle Macedo <manuecomp@yahoo.com.br>
2016-04-13 23:33:14 -0300

    ...

--- src/grammar.java
+++ src/grammar.java
@@ -8,22 +8,30 @@ public class grammar {
 	public int classStructure(ArrayList<Token> tokens, int i, Parsing parser){
 		if(tokens.get(i).getRepresentation().equals("class")){
 			if(tokens.get(i+1).getType().equals("identificador")){
-				this.expressionHeritage(tokens, i, parser);
-
+				i = this.expressionHeritage(tokens, i, parser);
+				if(tokens.get(i).getType().equals("{")){
+					i = this.contentClass(tokens, i, parser);
+					
+				}
 			}
 		}
 	}
 	
 	public int expressionHeritage(ArrayList<Token> tokens, int i, Parsing parser){
 		if(tokens.get(i).getRepresentation().equals(">")){
-			if(tokens.get(i).getType().equals("identificador")){
+			if(tokens.get(i+1).getType().equals("identificador")){
 				return i+1;
 			}
 		}else if(tokens.get(i).getRepresentation().equals(" ")){
 			return i+1;
 		}
 		else{
-			//erro de herança mal formada?
+			//erro
+			return i+1;
 		}
+	}
+	
+	public int contentClass(ArrayList<Token> tokens, int i, Parsing parser){
+		if
 	}
 }
