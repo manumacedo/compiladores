15a6865e7752b9cf88497fed55dfaa442e047321
Manuelle Macedo <manuecomp@yahoo.com.br>
2016-04-15 22:38:34 -0300

    agora sim, ta indo <3

--- src/grammar.java
+++ src/grammar.java
@@ -1,60 +1,120 @@
+import java.util.ArrayList;
 
 public class grammar {
+	
+	private int numberTokens = 0; // quantos tokens já foram lidos
+	private ArrayList<Token> tokens; //lista com todos os tokens recebidos, em ordem de ecscopo
+	private ArrayList<String> error; //lista com os erros sintaticos
+	private Token current; //token atual
 
+	/**
+	 * Método para pegar o proximo token a ser analisado
+	 * @return 
+	 * 
+	 */
+	public Token nextToken(){ 
+		if (numberTokens < tokens.size()) { 
+            return tokens.get(numberTokens++);
+        } else {
+        	
+            return new Token(-1, "End of File", null);  //cria um token de fim de arquivo. 
+        }
+	}
+	
+	/**
+	 * Metodo para comparar  e conduzir ao proximo token
+	 * @param expected
+	 */
+	
+	public void consumeToken(String expected){
+		if ((current.getRepresentation().equals(expected) || current.getType().equals(expected)) && (!current.getRepresentation().equals("End of File"))) { //verifica se o token atual e o que era esperado
+            current = nextToken();
+        } else {
+            //metodo que gera o erro  
+        }
+		
+	}
+	
+	/**
+	 * Metodo para reconhecer a estrutura do arquivo
+	 */
 	public void fileStructure(){
 		
 	}
 	
-	public int classStructure(ArrayList<Token> tokens, int i, Parsing parser){
-		if(tokens.get(i).getRepresentation().equals("class")){
-			if(tokens.get(i+1).getType().equals("identificador")){
-				i = this.expressionHeritage(tokens, i, parser);
-				if(tokens.get(i).getType().equals("{")){
-					i = this.contentClass(tokens, i, parser);
-					
-				}
-			}
+	/**
+	 * Metodo que reconhece as produções para estrutura de uma classe
+	 * 
+	 */
+	public void classStructure(){
+		 switch (current.getRepresentation()) {
+         case "class":
+        	 consumeToken("class");
+        	 consumeToken("identificador");
+        	 expressionHeritage();
+        	 consumeToken("{");
+        	 contentClass();
+        	 consumeToken("}");
+             //atual = new Simbolos();
+             //atual.setCategoria(Simbolos.CLASS);
+             //atual.setNome(proximo.getValor());
+             //escopo.addFilho(atual);
+            // Simbolos anterior = escopo; //salva o antigo escopo
+             //escopo = atual; //o novo escopo e a classe atual
+             //escopo = anterior; //volta o escopo para o pai da classe.
+             break;
+         default:
+             //metodo do erro sintatico
+             break;
+     }
+	}
+	/**
+	 * Metodo que reconhece a herança da classe
+	 * 
+	 */
+	public void expressionHeritage(){
+		switch (current.getRepresentation()) {
+        case ">":
+            consumeToken(">");
+            consumeToken("identificador");
+            break;
+        default:
+            break;
 		}
 	}
 	
-	public int expressionHeritage(ArrayList<Token> tokens, int i, Parsing parser){
-		if(tokens.get(i).getRepresentation().equals(">")){
-			if(tokens.get(i+1).getType().equals("identificador")){
-				return i+1;
-			}
-		}else if(tokens.get(i).getRepresentation().equals(" ")){
-			return i+1;
-		}
-		else{
-			//erro
-			return i+1;
-		}
+	/**
+	 * Metodo para reconhecer o conteudo da classe
+	 * 
+	 */
+	public void contentClass(){
+		switch (current.getRepresentation()) {
+        case "void":
+            recConst();
+            recConteudoClasse();
+            break;
+        case "const":
+            recIdDeclaracao();
+            recConteudoClasse();
+            break;
+        default:
+            if (proximo.getTipo().equals("palavra_reservada") || proximo.getTipo().equals("id")) {
+                recIdDeclaracao();
+                recConteudoClasse();
+                break;
+            } else if (!proximo.getValor().equals("}") && !proximo.getValor().equals("class")) { //recuperaçao do erro, verifica se acabou o bloco, ou surgiu outra classe
+                erroSintatico("falta declaraçao de variavel ou de metodo");
+                proximo = proximo();
+                recConteudoClasse();
+            }
+            break;
+    }
 	}
 	
-	public int contentClass(ArrayList<Token> tokens, int i, Parsing parser){
-		if(){
-			if(tokens.get(i).getType().equals("identificador")){
-				
-			}
-			
-		}
+	public void idDeclaration(ArrayList<Token> tokens, int i, Parsing parser){
+		
 	}
 	
-	public boolean idDeclaration(ArrayList<Token> tokens, int i, Parsing parser){
-		if(tokens.get(i).getType().equals("identificador")||tokens.get(i).getRepresentation().equals("int")||tokens.get(i).getRepresentation().equals("char") || tokens.get(i).getRepresentation().equals("bool")|| tokens.get(i).getRepresentation().equals("string")||tokens.get(i).getRepresentation().equals("float"){
-			if(tokens.get(i+1).getType().equals("identificador")){
-				if //complemento id
-			}
-		}
-	}
-	
-	public boolean idComplement(ArrayList<Token> tokens, int i, Parsing parser){
-		if(tokens.get(i).getRepresentation().equals("[")){
-			if(tokens.get(i+1).getType().equals("numero") || tokens.get(i).getType().equals("identificador")){
-				if(tokens.get(i+2).getRepresentation().equals("]")){
-					//lista vetor
-				}
-			}
-		}
+	public void idComplement(ArrayList<Token> tokens, int i, Parsing parser){
 	}
 }
